ae53fb3937679c3dfdf7031bca84c1f0e331ddfb
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-04-20 15:03:26 +0200

    ft: add onboarding screen

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile/screens/on_boarding.dart';
+import 'package:mobile/utils/theme.dart';
 
 void main() {
   runApp(const FNMApplication());
@@ -13,12 +16,12 @@ class FNMApplication extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Text("Welcome to FNM Store - CRUD Application"),
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme
         ),
+        elevatedButtonTheme: DTheme.elevatedButtonThemeData
       ),
+      home: const OnBoardingScreen()
     );
   }
 
---
+++ mobile/lib/screens/on_boarding.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/utils/colors.dart';
+import 'package:mobile/utils/text.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/onboarding.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            bottom: 100.0,
+            left: 30.0,
+            right: 30.0,
+            child: Column(
+              spacing: 28.0,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/logo.png",
+                  height: 56.0,
+                  width: 48.0,
+                ),
+                Column(
+                  spacing: 8.0,
+                  children: [
+                    Text(
+                      DTexts.welcome,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 48.0,
+                        height: 1,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      DTexts.welcomeDesc,
+                      style: TextStyle(
+                        color: DColors.descColor,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 67.0,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all<Color>(
+                          DColors.primaryGreen
+                      )
+                    ),
+                    onPressed: () => {},
+                    child: Text(
+                        DTexts.getStated
+                    )
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+}
