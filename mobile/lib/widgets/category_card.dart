8a6f7ecd9f72c40c6e6aba6a3f94b0eef96a6fc3
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-04-21 15:18:56 +0200

    refactor: update the category_card to accept props

--- mobile/lib/widgets/category_card.dart
+++ mobile/lib/widgets/category_card.dart
@@ -2,30 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:mobile/utils/colors.dart';
 
 class CategoryCard extends StatelessWidget {
-  const CategoryCard({super.key});
+  const CategoryCard({
+    super.key,
+    this.containerHeight = 52.0,
+    this.containerWidth = 52.0,
+    this.imageWidth = 28.0,
+    this.imageHeight = 28.0,
+    this.fontSize = 10.0
+  });
+
+  final double containerHeight;
+  final double containerWidth;
+  final double imageWidth;
+  final double imageHeight;
+  final double fontSize;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 5.0,
-      children: [
-        Container(height: 52.0, width: 52.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50.0),
-            color: Color(0xFFE6F2EA)
+    return IntrinsicHeight(
+      child: Column(
+        spacing: 5.0,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(height: containerHeight, width: containerWidth,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50.0),
+                  color: Color(0xFFE6F2EA)
+              ),
+              child: Center(
+                child: SizedBox(height: imageHeight, width: imageWidth,
+                  child: Image.asset("assets/images/leaf.png", fit: BoxFit.contain),
+                ),
+              )
           ),
-          child: Center(
-            child: SizedBox(height: 28, width: 28,
-              child: Image.asset("assets/images/leaf.png", fit: BoxFit.contain),
-            ),
-          )
-        ),
-        Text("Vegetables", style: TextStyle(
-          color: DColors.grey,
-          fontSize: 10.0,
-          fontWeight: FontWeight.w900
-        ))
-      ],
+          Text("Vegetables", style: TextStyle(
+              color: DColors.grey,
+              fontSize: fontSize,
+              fontWeight: FontWeight.w900
+          ))
+        ],
+      ),
     );
   }
 }
