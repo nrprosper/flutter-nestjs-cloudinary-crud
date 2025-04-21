a91e365176151ce64fe19279ec3fb34acdc1e994
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-04-21 18:16:34 +0200

    ft: add .env with base url and load it before the app starts

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile/screens/on_boarding.dart';
 import 'package:mobile/utils/theme.dart';
 
-void main() {
+void main() async{
+  await dotenv.load(fileName: ".env");
   runApp(const FNMApplication());
 }
 
