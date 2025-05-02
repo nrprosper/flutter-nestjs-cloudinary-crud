412383033fb8fb336f5de7fb333f9aa10e266305
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-05-02 09:30:26 +0200

    ft: add product service and viewmodel to be injected in app

--- mobile/lib/providers.dart
+++ mobile/lib/providers.dart
@@ -1,9 +1,12 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile/data/category.dart';
+import 'package:mobile/data/product.dart';
 import 'package:mobile/services/category_service.dart';
+import 'package:mobile/services/product_service.dart';
 import 'package:mobile/state/state.dart';
 import 'package:mobile/viewmodel/category_viewmodel.dart';
+import 'package:mobile/viewmodel/product_viewmodel.dart';
 
 final categoryServiceProvider = Provider<CategoryService>((ref) {
   return CategoryService();
@@ -13,4 +16,15 @@ final categoryServiceProvider = Provider<CategoryService>((ref) {
 final categoryViewModelProvider = StateNotifierProvider<CategoryViewModel, State<List<Category>>>((ref) {
   final categoryService = ref.watch(categoryServiceProvider);
   return CategoryViewModel(categoryService);
+});
+
+
+
+final productServiceProvider = Provider<ProductService>((ref) {
+  return ProductService();
+});
+
+final productViewModelProvider = StateNotifierProvider<ProductViewModel, State<List<Product>>>((ref) {
+  final productService = ref.watch(productServiceProvider);
+  return ProductViewModel(productService);
 });
